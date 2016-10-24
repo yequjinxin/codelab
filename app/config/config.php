42986d5e25fe3d5a2ebd0252ef8f75e34f3b5b58
unknown <liujun01@tkoffice.local>
2016-10-24 20:13:22 +0800

    db config

--- app/config/config.php
+++ app/config/config.php
@@ -7,6 +7,7 @@ $config = array(
             'pwd' => '',
             'host' => '127.0.0.1',
             'db' => 'codelab',
+            'dsn' => 'mysql:host=127.0.0.1;port=3306;dbname=codelab;charset=utf8;'
         )
     ),
     // weibo
