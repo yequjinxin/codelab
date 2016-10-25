3f95a52a926f2a83bd205a73f2ddd7c0d5faecbc
yequjinxin <669733506@qq.com>
2016-10-25 11:41:41 +0800

    f => F

---
+++ app/lib/File.php
@@ -0,0 +1,34 @@
+<?php
+namespace app\lib;
+
+class File {
+    static function deleteDir($path) {
+        $op = dir($path);
+        while (false !== ($item = $op->read())) {
+            if ($item == '.' || $item == '..') {
+                continue;
+            }
+            if (is_dir($op->path . '/' . $item)) {
+                self::deleteDir($op->path . '/' . $item);
+                rmdir($op->path . '/' . $item);
+            } else {
+                unlink($op->path . '/' . $item);
+            }
+        }
+    }
+
+    static function writeData($dir, $data = null) {
+        $pos = strrpos($dir, '/');
+        $prefix = substr($dir, 0, $pos);
+        if (!file_exists($prefix)) {
+            self::writeData($prefix);
+        }
+        if ($data !== null) {
+            file_put_contents($dir, $data);
+        } else {
+            mkdir($dir);
+        }
+    }
+}
+
+?>
