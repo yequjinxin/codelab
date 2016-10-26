4a56356c7684016e93a18c9f7e88cde860b4d11c
unknown <liujun01@tkoffice.local>
2016-10-26 17:50:37 +0800

    weibo config

--- app/config/weibo.php
+++ app/config/weibo.php
@@ -2,7 +2,7 @@
 
 $config = array(
     // weibo
-    'WB_AKEY' => '3416928080',
-    'WB_SKEY' => 'c23eba5f81922a0b377f1b08c496f027',
-    'WB_CALLBACK_URL' => 'http://c.ranlau.com/index.php?a=weiboCallback',
+    'WB_AKEY' => '',
+    'WB_SKEY' => '',
+    'WB_CALLBACK_URL' => '',
 );
