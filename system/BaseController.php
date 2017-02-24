b709b949905ff6ce79543c32289f92417cefe743
unknown <liujun01@tkoffice.local>
2017-02-24 14:46:08 +0800

    open

--- system/BaseController.php
+++ system/BaseController.php
@@ -7,13 +7,17 @@ class BaseController extends Controller {
         parent::__construct();
         if ($this->loginType === 'weibo'
             && (empty($this->user) || (isset($this->user['error']) && isset($this->user['error_code'])))) {
-           $this->isLogin = false;
+            $this->isLogin = false;
         } else {
-           $this->isLogin = true;
+            $this->isLogin = true;
         }
-
+        $this->isOpen = false;
         if (!$this->isLogin) {
-           $this->redirect('index.php?c=user&a=login');
+            if (checkProject()) {
+                $this->isOpen = true;
+            } else {
+                $this->redirect('index.php?c=user&a=login');
+            }
         }
     }
 
@@ -21,5 +25,19 @@ class BaseController extends Controller {
         $typeId = isset($this->user['id']) ? $this->user['id'] : 0;
         $user = $this->db->find("select * from user where type='weibo' and type_id='$typeId' and status not in (0,9)");
         return $user;
+    }
+
+    protected function checkProject() {
+        if ((empty($_GET['c']) || strtolower($_GET['c']) === 'index')
+            && strtolower($_GET['a']) === 'main') {
+            // 判断项目是否公开
+            $id = empty($_GET['pro_id']) ? 0 : $_GET['pro_id'];
+            $ret = $this->db->find("select * from project where id=$id");
+            if (!empty($ret) && $ret[0]['is_open']) {
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
 }
