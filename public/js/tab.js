4002ade89da60ab0a25b976eea59eef92cdd3817
unknown <liujun01@tkoffice.local>
2017-02-27 11:29:54 +0800

    dd

--- public/js/tab.js
+++ public/js/tab.js
@@ -39,6 +39,7 @@ define(['config', 'lib'], function (config, lib) {
                 e.setAttribute("aria-label", e.title);
                 return e;
             },
+            readOnly: $('#pro-is-open').val(),
         });
         return mirror;
     }
